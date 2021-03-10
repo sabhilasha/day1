28eb0859763a522106420860f74612140eea90d6
unknown <ibmjavanh02@iiht.tech>
2021-03-10 12:56:17 +0530

    string and date

---
+++ StringClass/src/DateClass.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+public class DateClass {
+	public static void main(String[] args) {
+		Date date1 = new Date(2021,3,10);
+		System.out.println(date1.getDate());
+		System.out.println(date1.getDay());
+		System.out.println(date1.getMonth());
+		System.out.println(date1.getYear());
+		
+		Date date2 = new Date(2021,3,10);
+		Date date3 = new Date();
+		System.out.println(date1==date2);
+		System.out.println(date1.equals(date2));
+		System.out.println(date1.equals(date3));
+		
+	}
+
+}
---
+++ StringClass/src/StringDemo.java
@@ -0,0 +1,35 @@
+
+public class StringDemo {
+	public static void main(String[] args) {
+	  //  usingStringPool();
+	//	withoutStringPool();
+	//	usingStringBuilder();
+		dataManipulation();
+	
+	}
+	private static void dataManipulation() {
+		String s1 = "IBM";
+		System.out.println(s1);
+		s1=s1.concat(", banglore");
+		System.out.println(s1);
+		
+	}
+	
+	private static void usingStringBuilder() {
+		StringBuilder s1 = new StringBuilder("IBM");
+		StringBuilder s2 = new StringBuilder("IBM");
+		System.out.println(s1 ==s2);
+	}
+	private static void withoutStringPool() {
+		String s1 = new String("IBM");
+		String s2 = new String("IBM");
+		System.out.println(s1 ==s2);
+		
+	}
+	private static void usingStringPool() {
+	String s1 = "IBM";
+	String s2 = "IBM";
+	System.out.println(s1 ==s2);
+	
+	}
+}
